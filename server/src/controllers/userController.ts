9104a5f53f24796bc0d664a8ad6d01de2192b13a
porat6766 <porat850@gmail.com>
2025-01-02 15:45:00 +0200

    server commit

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express";
 import User from "../models/userModel";
 import { IUser } from "../types/userTypes";
 import { AuthenticatedRequest } from "../types/expressTypes";
+import Joi from "joi";
 
 export const getUserById = async (
   req: AuthenticatedRequest,
@@ -11,6 +12,7 @@ export const getUserById = async (
 ): Promise<void> => {
   try {
     const userId = req.user?.id;
+
     if (!userId) {
       res.status(400).json({ message: "User not authenticated" });
       return;
@@ -41,52 +43,66 @@ export const getUserById = async (
 
 export const signUp = async (req: Request, res: Response): Promise<void> => {
   try {
-    const {
-      username,
-      email,
-      password,
-    }: { username: string; email: string; password: string } = req.body;
+    const schema = Joi.object({
+      username: Joi.string().min(3).max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required(),
+    });
+
+    const { error } = schema.validate(req.body);
+    if (error) {
+      res.status(400).json({ message: error.details[0].message });
+      return;
+    }
+
+    const { username, email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      res.status(400).json({ message: "User already exists." });
+      res.status(400).json({ message: "Email is already registered." });
       return;
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser: IUser = new User({
+    const newUser = await User.create({
       username,
       email,
       password: hashedPassword,
     });
 
-    await newUser.save();
-
     const token = jwt.sign(
       { id: newUser._id },
       process.env.JWT_SECRET as string,
       {
-        expiresIn: "1h",
+        expiresIn: "3h",
       }
     );
 
-    if (newUser.password) {
-      newUser.password = "";
-    }
+    const userResponse = {
+      id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      createdAt: newUser.createdAt,
+      role: newUser.role,
+    };
 
+    // Send cookie with token
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV === "production",
-      maxAge: 3600000,
+      maxAge: 3600000, // 1 hour
       sameSite: "strict",
     });
 
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: newUser });
+    res.status(201).json({
+      message: "User created successfully",
+      user: userResponse,
+    });
+    console.log("user created sucessefully", userResponse);
   } catch (err) {
-    res.status(500).json({ message: "Server error", error: err });
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -110,16 +126,22 @@ export const logIn = async (req: Request, res: Response): Promise<void> => {
       expiresIn: "3h",
     });
 
-    user.password = "";
+    const userResponse = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      createdAt: user.createdAt,
+      role: user.role,
+    };
 
     res.cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV === "production",
       maxAge: 3600000,
       sameSite: "strict",
     });
 
-    res.status(200).json({ message: "Login successful", user });
+    res.status(200).json({ message: "Login successful", userResponse });
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err });
   }
@@ -131,18 +153,26 @@ export const updateUser = async (
 ): Promise<void> => {
   try {
     const userId = req.params.id;
+    console.log(userId);
+
     const {
       username,
       email,
       password,
-    }: { username?: string; email?: string; password?: string } = req.body;
+      googleId,
+      profileImage,
+      role,
+      sites,
+    }: IUser = req.body;
 
-    const user: IUser | null = await User.findById(userId);
+    // חיפוש המשתמש לפי ID
+    const user = await User.findById(userId);
     if (!user) {
       res.status(404).json({ message: "User not found." });
       return;
     }
 
+    // עדכון הסיסמה אם נמסרה
     if (password) {
       const hashedPassword = await bcrypt.hash(password, 10);
       user.password = hashedPassword;
@@ -150,6 +180,10 @@ export const updateUser = async (
 
     if (username) user.username = username;
     if (email) user.email = email;
+    if (googleId) user.googleId = googleId;
+    if (profileImage) user.profileImage = profileImage;
+    if (role) user.role = role;
+    if (sites) user.sites = sites;
 
     await user.save();
 
@@ -162,7 +196,7 @@ export const updateUser = async (
 export const logOut = async (req: Request, res: Response): Promise<void> => {
   try {
     res.clearCookie("token", {
-      httpOnly: true,
+      httpOnly: false,
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
     });
@@ -172,6 +206,7 @@ export const logOut = async (req: Request, res: Response): Promise<void> => {
     res.status(500).json({ message: "Server error", error: err });
   }
 };
+
 export const deleteUser = async (
   req: AuthenticatedRequest,
   res: Response
