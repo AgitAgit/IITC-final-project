c26b97db77c79c113c48b3f84375221d8471567f
eden shabi <eden9876102@gmail.com>
2025-01-06 11:18:15 +0200

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Layout from "./pages/Layout";
 import Error from "./pages/Error";
+import TemplatesPage from "./pages/Templates";
 
 import "./App.css";
 
@@ -14,19 +15,10 @@ function App() {
           path: "/",
           element: <Landing />,
         },
-        // {
-        //   path: "/About",
-        //   element: <About />,
-        // },
-        // {
-        //   path: "/Profile/:id",
-        //   element: <Profile />,
-        // },
-        // {
-        //   path: "/Chat",
-        //   element: <Chat />
-
-        // },
+        {
+          path: "/templates",
+          element: <TemplatesPage />,
+        },
         {
           path: "*",
           element: <Error />,
