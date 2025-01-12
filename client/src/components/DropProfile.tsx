b7de7fd6d197ed0c3853e2fa7a2e6831d55583f5
porat6766 <porat850@gmail.com>
2025-01-12 12:04:38 +0200

    last

--- client/src/components/DropProfile.tsx
+++ client/src/components/DropProfile.tsx
@@ -54,12 +54,12 @@ function ProfileDropdown() {
           <span className="text-white text-xl ml-4">
             {userData?.user?.profileImage ? (
               <img
-                className="min-w-10 rounded-full aspect-square object-cover"
+                className="min-w-12 rounded-full aspect-square object-cover"
                 alt="photo profile"
                 src={userData.user.profileImage}
               />
             ) : (
-              <span className="w-10 h-10 rounded-full bg-black text-white font-bold text-2xl flex items-center justify-center pb-1">
+              <span className="w-12 h-12 rounded-full bg-black text-white font-bold text-2xl flex items-center justify-center pb-1">
                 {userData?.user?.firstName?.charAt(0).toUpperCase()}
               </span>
             )}
